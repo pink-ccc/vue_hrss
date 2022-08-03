db75b51a6c2a7ea1423b8576d6661158ed0278bd
赵虎虎 <2291266472@qq.com>
2022-08-03 16:33:01 +0800

    项目初始化完毕

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+    <router-view />
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+// 使用 $ 定义变量
+// $highlight-color: pink;
+// $basic-border: 1px solid black;
+
+// #app {
+//   background-color: $highlight-color;
+//   border: $basic-border;
+// }
+</style>
---
+++ src/store/modules/user.js
@@ -0,0 +1,19 @@
+const state = () => {
+  return {
+
+  }
+}
+
+const actions = {
+
+}
+const mutations = {
+
+}
+export default {
+  // 开启命名空间
+  namespaced: true,
+  state,
+  actions,
+  mutations
+}
